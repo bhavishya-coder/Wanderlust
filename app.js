e3d63a9c3c84e81209a6229f6e97e08f06d8e3fd
Bhavishya Singh <129263007+bhavishya-coder@users.noreply.github.com>
2025-03-25 00:36:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ store.on("error",()=>{
 
 const sessionOptions =  {
   store,
-  secret: process.env.secret,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
